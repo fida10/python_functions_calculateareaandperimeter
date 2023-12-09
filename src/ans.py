ea45457a6deb4f13c5be98733055fc4431df2914
eshad <fidaeshad10@gmail.com>
2023-12-09 10:32:43 -0600

    added question

--- src/ans.py
+++ src/ans.py
@@ -1,3 +1,11 @@
+""" 
+Practice Question 1: Calculate Area and Perimeter
+
+Task:
+
+Write a function calculate_area_perimeter that takes the length and width of a rectangle and returns both the area and the perimeter.
+"""
+
 def calculate_area_perimeter(b, h):
     return b * h, (2 * b) + (2 * h)
     
